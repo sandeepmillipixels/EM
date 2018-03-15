a9d5624d7b1f97cba8b0528641d58d8cf78283c9
sandeepmillipixels <sandeep.singh@millipixels.com>
2018-03-15 09:56:41 +0530

    New Theme Implementation

---
+++ app/src/main/java/com/example/millipixelsinteractive_031/em/adapter/PagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.millipixelsinteractive_031.em.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+
+/**
+ * Created by millipixelsinteractive_031 on 13/03/18.
+ */
+class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+
+
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
+}
