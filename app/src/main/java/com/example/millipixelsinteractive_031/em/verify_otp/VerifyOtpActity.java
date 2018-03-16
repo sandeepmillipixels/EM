b80c88d7977f6516fb3b97a7ee3e16d6a9414fce
sandeepmillipixels <sandeep.singh@millipixels.com>
2018-03-16 17:22:32 +0530

    Click and save slip photo

--- app/src/main/java/com/example/millipixelsinteractive_031/em/verify_otp/VerifyOtpActity.java
+++ app/src/main/java/com/example/millipixelsinteractive_031/em/verify_otp/VerifyOtpActity.java
@@ -46,28 +46,6 @@ public class VerifyOtpActity extends AppCompatActivity {
         setContentView(R.layout.activity_verify_otp);
         ButterKnife.bind(this);
 
-
-//        OTPReceiver.bindListener(new SmsView() {
-//
-//            @Override
-//            public void autoFillOTP(String otp) {
-//
-//                    char first = otp.charAt(0);
-//                    char second = otp.charAt(1);
-//                    char third = otp.charAt(2);
-//                    char fourth = otp.charAt(3);
-//
-//                    first_digit_EditText.setText(String.valueOf(first));
-//                    second_digit_EditText.setText(String.valueOf(second));
-//                    third_digit_EditText.setText(String.valueOf(third));
-//                    fourth_digit_EditText.setText(String.valueOf(fourth));
-//
-//
-//                Log.e("OTP",otp);
-
-//            }
-//        });
-
     }
     @OnClick(R.id.btnChangeNumber)
     public void onChangeNumber(){
